4a71be8e348807fc4657b95810b38925e1764ceb
Rama Hany <hanyrama703@gmai.com>
2024-03-16 22:01:18 +0200

    modified connection

--- connection.php
+++ connection.php
@@ -6,4 +6,8 @@ $pass = "";
 $db = "islamic_clothes_shop";
 
 $con = mysqli_connect($host, $user, $pass, $db);
+if ($con->connect_error) {
+  die("Connection Failed: " . $con->connect_error);
+}
+echo "";
 $con->set_charset("utf8");
